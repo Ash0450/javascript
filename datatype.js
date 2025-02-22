38fe6db2ef1bc8923f1a2bb99274402d825759ad
Ash0450 <malviya.ashish22@gmail.com>
2025-02-22 10:48:36 +0530

    Initial commit

---
+++ datatype.js
@@ -0,0 +1,26 @@
+// Premitive data types in JavaScript
+// 1. Number
+let a =2;
+console.log(a) // 2
+// 2. String
+let b="Hello";
+console.log(b) // Hello
+// 3. Boolean
+let c=true;
+console.log(c) // true
+// 4. Undefined
+let d;    
+console.log(d) // undefined
+// 5. Null
+let e=null;
+console.log(e) // null
+// 6. Symbol
+let f=Symbol("foo");
+console.log(f) // Symbol(foo)
+// 7. BigInt
+let g=1234567890123456789012345678901234567890n;        
+console.log(g) // 1234567890123456789012345678901234567890n
+//
+
+
+console.log('Reference data types in JavaScript')
